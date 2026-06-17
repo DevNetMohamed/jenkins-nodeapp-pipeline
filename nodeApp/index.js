ac51a9765c23a89ba6949e3ecd8c16804c83fef6
Mohamed Adel <mohamedadelhareidy@gmail.com>
2026-06-18 01:48:16 +0300

    Update

--- nodeApp/index.js
+++ nodeApp/index.js
@@ -1,6 +1,11 @@
 import express from 'express'
 const app =  express();
-
-app.listen(3000, ()=>{
+const port = 4000;
+app.use("/", ()=>{
     return "Hello from Node.js"
+})
+
+app.listen(port, ()=>{
+    console.log(`server is running on port ${port}`);
+    
 })
